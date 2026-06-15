f62f234aa840494e5f5fe448bb68c8799a25f51c
emontecer <emontecer@gmail.com>
2026-06-15 10:56:48 +0200

    Add license information to multiple PHP files
    This commit adds the AFL-3.0 Academic Free License 3.0 notice to the header of various PHP files across the project. This change ensures that all relevant files are compliant with the licensing requirements.
    
    Files modified:
    - config/index.php
    - controllers/admin/AdminMdfcforpsController.php
    - controllers/admin/index.php
    - controllers/front/ajax.php
    - controllers/front/Feed.php
    - controllers/front/index.php
    - controllers/index.php
    - index.php
    - mdfcforps.php
    - sql/index.php
    - src/Controller/Admin/FeedController.php
    - src/Controller/Admin/index.php
    - src/Controller/index.php
    - src/Grid/Data/HubSalesDataFactory.php
    - src/Grid/Data/ProductCatalogDataDecorator.php
    - src/Grid/Data/ProductFeedDataDecorator.php
    - src/Grid/Data/SalesDataDecorator.php
    - src/Grid/Data/index.php
    - src/Grid/Definition/Factory/ProductCatalogGridDefinitionFactory.php
    - src/Grid/Definition/Factory/ProductFeedGridDefinitionFactory.php
    - src/Grid/Definition/Factory/SalesGridDefinitionFactory.php
    - src/Grid/Definition/Factory/index.php
    - src/Grid/Definition/index.php
    - src/Grid/Query/ProductCatalogQueryBuilder.php
    - src/Grid/Query/ProductFeedQueryBuilder.php
    - src/Grid/Query/SalesQueryBuilder.php
    - src/Grid/Query/index.php
    - src/Grid/index.php
    - src/Install/Installer.php
    - src/Install/index.php
    - src/Repository/SaleRepository.php
    - src/Repository/index.php
    - src/Service/AttributionService.php
    - src/Service/FeedEligibilityService.php
    - src/Service/FeedProductsService.php
    - src/Service/FeedService.php
    - src/Service/HubClient.php
    - src/Service/ModuleConfig.php
    - src/Service/index.php
    - src/index.php
    - tests/Unit/Service/AttributionServiceTest.php
    - tests/Unit/Service/index.php
    - tests/Unit/index.php
    - tests/bootstrap.php
    - tests/index.php
    - tests/phpstan/bootstrap.php
    - tests/phpstan/index.php
    - translations/index.php
    - upgrade/index.php
    - upgrade/upgrade-1.0.0.php
    - views/css/index.php
    - views/img/index.php
    - views/index.php
    - views/js/admin/index.php
    - views/js/front/index.php
    - views/js/index.php
    - views/templates/admin/index.php
    - views/templates/admin/mdfcforps/index.php
    - views/templates/hook/index.php
    - views/templates/index.php

--- tests/Unit/Service/AttributionServiceTest.php
+++ tests/Unit/Service/AttributionServiceTest.php
@@ -4,6 +4,7 @@
  *
  * @author Marques de France
  * @copyright Copyright (c) Marques de France
+ * @license   AFL-3.0 Academic Free License 3.0
  */
 
 declare(strict_types=1);
--- tests/Unit/index.php
+++ tests/Unit/index.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Security guard file.
  *
  * @author Marques de France
  * @copyright Copyright (c) Marques de France
+ * @license   AFL-3.0 Academic Free License 3.0
  */
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
